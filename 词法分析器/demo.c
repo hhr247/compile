18ad3c935879851cccfcf38151ac2469888c8a85
hhr247 <101495510+hhr247@users.noreply.github.com>
2022-03-17 18:09:34 +0800

    Add files via upload

---
+++ 词法分析器/demo.c
@@ -0,0 +1,33 @@
+int main(void){
+    //测试注释
+    char* str = "123";//这是注释
+    //行注释
+    /*
+    块注释
+
+    */
+    //测试实数
+    int a = 0x13f;
+    int a = 0x13;
+    int a = 000013;
+    int a = 0b11;
+    int a = 11;
+    double a = 0.12;
+    double a = .12;
+    double b = 1e+10;
+    double b = 1.0e10;
+    double b = .1e-10;
+    double b = 18.1;
+    //测试字符串
+    char s = 'c';
+    char s = '\n';
+    char* str = "char\n\"";
+    //测试不同数量的界符
+    a=a<=b?a<<1:a<<2;
+    a<<=1;
+    b++;
+    if(!a){
+        return 0;
+    }
+    return a+b;
+}
